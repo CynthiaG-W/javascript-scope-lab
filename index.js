15d2cf2096d5922299b577d1831f233121a76074
CynthiaG-W <cindydetec@gmail.com>
2026-04-08 13:05:42 +0300

    commit javascript-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Global Scope Variables
+const burgers = ['Hamburger', 'Cheeseburger']; // global array of burgers
+let featuredDrink = 'Strawberry Milkshake';    // global string for featured drink
+
+// Function to Add a Burger
+function addBurger() {
+  const newBurger = 'Flatburger'; // function-scoped variable
+  burgers.push(newBurger);        // add it to the global array
+}
+
+// Block-Scoped Burger Addition
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger'; // block-scoped variable
+  burgers.push(anotherNewBurger);               // add it to the global array
+}
+
+// Function to Change Featured Drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake'; // updates the global variable
+}
+
+// Block-Scoped Burger Addition
+function addBlockBurger() {
+  if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger'; // block-scoped variable
+    burgers.push(anotherNewBurger);               // add it to the global array
+  }
+} 
